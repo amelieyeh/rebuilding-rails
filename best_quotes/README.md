48561d5de7183cc71d134c88a855576af226377d
Amelie Yeh <amelieyeh@gmail.com>
2016-04-23 23:59:47 +0800

    Init project

---
+++ best_quotes/README.md
@@ -0,0 +1,2 @@
+# best_quotes
+best_quotes
---
+++ rulers/spec/rulers_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Rulers do
+  it 'has a version number' do
+    expect(Rulers::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
