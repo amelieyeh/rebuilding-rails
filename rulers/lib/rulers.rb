b829a719b3c4b03eeff184a99619ca514223f801
Amelie Yeh <amelieyeh@gmail.com>
2016-04-25 21:26:20 +0800

    Exercise 1 - Add debug.txt in rulers/lib/rulers.rb

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -2,6 +2,7 @@ require "rulers/version"
 
 module Rulers
   class Application
+    `echo debug > debug.txt`
     def call(env)
       [200, {'Content-Type' => 'text/html'},
         ["Hello from Ruby on Rulers!"]]
