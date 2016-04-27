28f720dbaf0ad3c74695c05206a28b01ed8bf954
Amelie Yeh <amelieyeh@gmail.com>
2016-04-27 23:53:28 +0800

    Chapter-2-tutorials

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -1,13 +1,31 @@
 require "rulers/version"
 require "rulers/array"
+require "rulers/routing"
 
 module Rulers
   class Application
     # `echo debug > debug.txt`
     def call(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [200, {'Content-Type' => 'text/html'},
-      ["Hello from Ruby on Rulers!"]]
+      [text]]
       puts "Sum an array: #{[2, 4, 6].sum}"
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
