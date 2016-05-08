86e298ce699a396c9b2690498500d0f959f6a88b
Amelie Yeh <amelieyeh@gmail.com>
2016-05-08 14:49:50 +0800

    Chapter-3-excerse-1-dit-it-work-class-check

--- rulers/lib/rulers/dependencies.rb
+++ rulers/lib/rulers/dependencies.rb
@@ -1,6 +1,13 @@
 class Object
   def self.const_missing(c)
-    require Rulers.to_underscore(c.to_s)
-    Object.const_get(c)
+    return nil if @calling_const_missing
+
+    @calling_const_missing = true
+    irequire Rulers.to_underscore(c.to_s)
+    klass = Object.const_get(c)
+
+    @calling_const_missing = false
+
+    klass
   end
 end
